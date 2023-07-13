37cb9185014000ea525b5f0233ee32a35c3816b2
abhijain1705 <abhijain3002@gmail.com>
2023-07-13 11:37:30 +0530

    create flutter project

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databseId = '64af9328c2e16ce48103';
+  static const String projectId = '64aecc1a3de63eeb16e8';
+  static const String endPoint = 'https://cloud.appwrite.io/v1';
+}
